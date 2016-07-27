2ed667f9e723aaf2d95d2b46c0760894cbe803da
Durran Jordan <durran@gmail.com>
2016-07-27 12:52:58 +0200

    INT-1686/INT-1457: Registry refactor:
    - Changed ComponentRegistry to AppRegistry.
    - Actions, Components, and Stores are now split within the registry.
    - Use objects instead of arrays for lookup performance.
    - Removed all the excess role/container nonsense.

---
+++ test/app-registry.test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+require('./helper');
+
+const expect = require('chai').expect;
+const Action = require('../lib/actions');
+const AppRegistry = require('../lib/app-registry');
+
+describe('AppRegistry', function() {
+  describe('#registerAction', function() {
+    var registry = null;
+
+    beforeEach(function() {
+      registry = new AppRegistry().registerAction('TestAction', 'testing');
+    });
+
+    it('registers the action', function() {
+      expect(registry.actions.TestAction).to.equal('testing');
+    });
+
+    it('publishes an action registered action', function(done) {
+      var unsubscribe = Action.actionRegistered.listen(function(name) {
+        expect(name).to.equal('TestAction');
+        unsubscribe();
+        done();
+      });
+    });
+  });
+
+  describe('#registerComponent', function() {
+    var registry = null;
+
+    beforeEach(function() {
+      registry = new AppRegistry().registerComponent('IndexView', 'testing');
+    });
+
+    it('registers the component', function() {
+      expect(registry.components.IndexView).to.equal('testing');
+    });
+
+    it('publishes a component registered action', function(done) {
+      var unsubscribe = Action.componentRegistered.listen(function(name) {
+        expect(name).to.equal('IndexView');
+        unsubscribe();
+        done();
+      });
+    });
+  });
+
+  describe('#registerStore', function() {
+    var registry = null;
+
+    beforeEach(function() {
+      registry = new AppRegistry().registerStore('IndexStore', 'testing');
+    });
+
+    it('registers the store', function() {
+      expect(registry.stores.IndexStore).to.equal('testing');
+    });
+
+    it('publishes a store registered action', function(done) {
+      var unsubscribe = Action.storeRegistered.listen(function(name) {
+        expect(name).to.equal('IndexStore');
+        unsubscribe();
+        done();
+      });
+    });
+  });
+
+  describe('#deregisterAction', function() {
+    context('when the action exists', function() {
+      var registry = null;
+
+      beforeEach(function() {
+        registry = new AppRegistry()
+          .registerAction('TestAction', 'testing')
+          .deregisterAction('TestAction');
+      });
+
+      it('deregisters the action', function() {
+        expect(registry.actions.TestAction).to.equal(undefined);
+      });
+
+      it('publishes an action deregisted action', function(done) {
+        var unsubscribe = Action.actionDeregistered.listen(function(name) {
+          expect(name).to.equal('TestAction');
+          unsubscribe();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#deregisterComponent', function() {
+    context('when the component exists', function() {
+      var registry = null;
+
+      beforeEach(function() {
+        registry = new AppRegistry()
+          .registerComponent('TestComponent', 'testing')
+          .deregisterComponent('TestComponent');
+      });
+
+      it('deregisters the component', function() {
+        expect(registry.components.TestComponent).to.equal(undefined);
+      });
+
+      it('publishes a component deregisted action', function(done) {
+        var unsubscribe = Action.componentDeregistered.listen(function(name) {
+          expect(name).to.equal('TestComponent');
+          unsubscribe();
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#deregisterStore', function() {
+    context('when the store exists', function() {
+      var registry = null;
+
+      beforeEach(function() {
+        registry = new AppRegistry()
+          .registerStore('TestStore', 'testing')
+          .deregisterStore('TestStore');
+      });
+
+      it('deregisters the store', function() {
+        expect(registry.stores.TestStore).to.equal(undefined);
+      });
+
+      it('publishes a store deregisted action', function(done) {
+        var unsubscribe = Action.storeDeregistered.listen(function(name) {
+          expect(name).to.equal('TestStore');
+          unsubscribe();
+          done();
+        });
+      });
+    });
+  });
+});
